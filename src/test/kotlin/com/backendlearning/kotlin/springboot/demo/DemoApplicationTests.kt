d51a66e169caa0b5699a0d14c2c10456f5a2be95
Rodrigo <rodrigottra.dev@gmail.com>
2022-06-11 14:13:38 +0200

    Downloaded springboot demo

---
+++ src/test/kotlin/com/backendlearning/kotlin/springboot/demo/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.backendlearning.kotlin.springboot.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
